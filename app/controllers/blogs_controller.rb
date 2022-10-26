f0fad79465484e802537c22213ac762fc9d86bd9
fumoko <tengtian.2213@gmail.com>
2022-10-26 23:42:50 +0900

    create app

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -31,7 +31,7 @@ class BlogsController < ApplicationController
     
     def destroy
         @blog = Blog.find(params[:id])
-        blog.destroy
+        @blog.destroy
         @blog.destroy
             redirect_to blogs_path, notice:"削除しました"
     end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,14 @@
 class UsersController < ApplicationController
 
-
+    before_action :set_beginning_of_week
+    
     def show
         @user = User.find(params[:id])    
     end
 
+    private
 
+    def set_beginning_of_week
+        Date.beginning_of_week = :sunday
+    end
 end
